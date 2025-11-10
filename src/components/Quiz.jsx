dcd1dfaa8f02a94c2d3a1a40ff293e976a373fce
Maria <73496077+MariaShut@users.noreply.github.com>
2025-11-10 20:46:55 +0300

    Refactor: Move state down to components

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,33 +5,17 @@ import { Question } from './Question.jsx';
 import quizCompleteImg from '../assets/quiz-complete.png';
 
 export const Quiz = () => {
-  const [answerState, setAnswerState] = useState('');
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex =
-    answerState === '' ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length; // can't exceed the number of questions
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
     selectedAnswer
   ) {
-    setAnswerState('answered');
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-
-    setTimeout(() => {
-      if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-        setAnswerState('correct');
-      } else {
-        setAnswerState('wrong');
-      }
-
-      // setting timer to change color of selected answer, checking.
-      setTimeout(() => {
-        setAnswerState('');
-      }, 2000);
-    }, 1000);
   },
   []);
 
@@ -53,11 +37,8 @@ export const Quiz = () => {
     <div id="quiz">
       <Question
         key={activeQuestionIndex}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
+        index={activeQuestionIndex}
         onSelectAnswer={handleSelectAnswer}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
-        answerState={answerState}
         onSkipAnswer={handleSkipAnswer}
       />
     </div>
